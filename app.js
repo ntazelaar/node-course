92fab781b28c914ac433d34dfd9688fe39d4a97e
Nick Tazelaar <nick@nicktazelaar.com>
2021-07-27 21:21:42 +0200

    Completed Section 3 Lesson 6

--- app.js
+++ app.js
@@ -4,5 +4,5 @@ const chalk = require('chalk')
 const msg = getNotes()
 console.log(msg)
 
-const greenMessage = chalk.green.inverse.bold('Success!')
+const greenMessage = chalk.blue.inverse.bold('Success!')
 console.log(greenMessage)
