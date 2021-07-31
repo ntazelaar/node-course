1bd83cec01e3a54877a13601e50596fba4d40771
Nick Tazelaar <nick@nicktazelaar.com>
2021-07-31 21:31:36 +0200

    Completed Section 7 Lesson 6

--- web-server/src/app.js
+++ web-server/src/app.js
@@ -4,7 +4,30 @@ const path = require('path')
 const app = express()
 const publicDirectoryPath = path.join(__dirname, '../public')
 
+app.set('view engine', 'hbs')
 app.use(express.static(publicDirectoryPath))
+
+app.get('', (request, response) => {
+    response.render('index', {
+        title: 'Weather App',
+        name: 'Nick Tazelaar'
+    })
+})
+
+app.get('/about', (request, response) => {
+    response.render('about', {
+        title: 'About Me',
+        name: 'Nick Tazelaar'
+    })
+})
+
+app.get('/help', (request, response) => {
+    response.render('help', {
+        title: 'Help',
+        message: 'How can I help?'
+    })
+})
+
 
 app.get('/weather', (request, response) => {
     response.send({
