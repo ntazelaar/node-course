b9798576e9dcc8ead9163f19644d70dadd594dc5
Nick Tazelaar <nick@nicktazelaar.com>
2021-07-31 22:11:51 +0200

    Completed Section 7 Lesson 9

--- web-server/src/app.js
+++ web-server/src/app.js
@@ -1,15 +1,18 @@
 const express = require('express')
 const path = require('path')
+const hbs = require('hbs')
 
 const app = express()
 
 // Define paths for Express configuration
 const publicDirectoryPath = path.join(__dirname, '../public')
-const viewsPath = path.join(__dirname, '../templates')
+const viewsPath = path.join(__dirname, '../templates/views')
+const partialsPath = path.join(__dirname, '../templates/partials')
 
 // Setup handlebars engine and views location
 app.set('view engine', 'hbs')
 app.set('views', viewsPath)
+hbs.registerPartials(partialsPath)
 
 // Setup static directory to serve
 app.use(express.static(publicDirectoryPath))
@@ -31,7 +34,24 @@ app.get('/about', (request, response) => {
 app.get('/help', (request, response) => {
     response.render('help', {
         title: 'Help',
-        message: 'How can I help?'
+        message: 'How can I help?',
+        name: 'Nick Tazelaar'
+    })
+})
+
+app.get('/help/*', (request, response) => {
+    response.render('404', {
+        title: '404',
+        name: 'Nick Tazelaar',
+        errorText: 'Help article not found!'
+    })
+})
+
+app.get('*', (request, response) => {
+    response.render('404', {
+        title: '404',
+        name: 'Nick Tazelaar',
+        errorText: 'Page not found'
     })
 })
 
