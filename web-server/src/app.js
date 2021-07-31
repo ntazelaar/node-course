d54def700339e73df92e19a92ccdf053e078e094
Nick Tazelaar <nick@nicktazelaar.com>
2021-07-31 21:38:11 +0200

    Completed Section 7 Lesson 7

--- web-server/src/app.js
+++ web-server/src/app.js
@@ -2,9 +2,16 @@ const express = require('express')
 const path = require('path')
 
 const app = express()
-const publicDirectoryPath = path.join(__dirname, '../public')
 
+// Define paths for Express configuration
+const publicDirectoryPath = path.join(__dirname, '../public')
+const viewsPath = path.join(__dirname, '../templates')
+
+// Setup handlebars engine and views location
 app.set('view engine', 'hbs')
+app.set('views', viewsPath)
+
+// Setup static directory to serve
 app.use(express.static(publicDirectoryPath))
 
 app.get('', (request, response) => {
