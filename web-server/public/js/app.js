59ecbb22d5d782939d29318247446b0f8662eeb0
Nick Tazelaar <nick@nicktazelaar.com>
2021-08-02 12:39:37 +0200

    Completed Section 8 Lesson 5

--- web-server/public/js/app.js
+++ web-server/public/js/app.js
@@ -1,1 +1,14 @@
 console.log('Client side javascript file is loaded!')
+
+fetch('http://localhost:3000/weather?address=!').then((response) => {
+    response.json().then((data) => {
+        if (data.error) {
+            return console.error(data.error)
+        }
+
+        console.log(data.location);
+        console.log(data.forecast);
+    }).catch((reason) => {
+        console.error('Error: ' + reason)
+    })
+})
