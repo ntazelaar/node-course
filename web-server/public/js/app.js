4ec6bb14e46f0fd0d86065333632d174c82fd6e0
Nick Tazelaar <nick@nicktazelaar.com>
2021-08-02 14:33:41 +0200

    Completed Section 8 Lesson 7

--- web-server/public/js/app.js
+++ web-server/public/js/app.js
@@ -1,14 +1,28 @@
 console.log('Client side javascript file is loaded!')
 
-fetch('http://localhost:3000/weather?address=!').then((response) => {
-    response.json().then((data) => {
-        if (data.error) {
-            return console.error(data.error)
-        }
+const weatherForm = document.querySelector('form')
+const search = document.querySelector('input')
+const messageOne = document.querySelector('#message-1')
+const messageTwo = document.querySelector('#message-2')
 
-        console.log(data.location);
-        console.log(data.forecast);
-    }).catch((reason) => {
-        console.error('Error: ' + reason)
+weatherForm.addEventListener('submit', (event) => {
+    event.preventDefault()
+
+    messageOne.textContent = 'Loading...'
+    messageTwo.textContent = ''
+
+    const location = search.value
+    fetch('http://localhost:3000/weather?address=' + location).then((response) => {
+        response.json().then((data) => {
+            if (data.error) {
+                messageOne.textContent = data.error
+                return
+            }
+
+            messageOne.textContent = data.location
+            messageTwo.textContent = data.forecast
+        }).catch((reason) => {
+            console.error('Error: ' + reason)
+        })
     })
 })
