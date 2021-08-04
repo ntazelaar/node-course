91dbbb263f1da3fd8610f3e222ce5a89c5096f11
Nick Tazelaar <nick@nicktazelaar.com>
2021-08-04 21:01:26 +0200

    Completed Section 12 Lesson 4

--- task-manager/src/routers/user.js
+++ task-manager/src/routers/user.js
@@ -14,6 +14,15 @@ router.post('/users', async (req, res) => {
     }
 })
 
+router.post('/users/login', async (req, res) => {
+    try {
+        const user = await User.findByCredentials(req.body.email, req.body.password)
+        res.send(user)
+    } catch (error) {
+        res.status(400).send()
+    }
+})
+
 router.get('/users', async (req, res) => {
     try {
         const users = await User.find({})
@@ -57,7 +66,7 @@ router.patch('/users/:id', async (req, res) => {
 
         res.send(user)
     } catch (error) {
-        res.send(400).send(e)
+        res.send(400).send(error)
     }
 })
 
