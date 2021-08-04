39512f856f8570f9a75f90d051d759c048cc2f41
Nick Tazelaar <nick@nicktazelaar.com>
2021-08-04 21:58:41 +0200

    Completed Section 12 Lesson 6

--- task-manager/src/routers/user.js
+++ task-manager/src/routers/user.js
@@ -7,8 +7,10 @@ router.post('/users', async (req, res) => {
     const user = new User(req.body)
 
     try {
+        const token = await user.generateAuthToken()
         await user.save()
-        res.status(201).send(user)
+
+        res.status(201).send({ user, token })
     } catch (error) {
         res.status(400).send(error)
     }
@@ -17,7 +19,8 @@ router.post('/users', async (req, res) => {
 router.post('/users/login', async (req, res) => {
     try {
         const user = await User.findByCredentials(req.body.email, req.body.password)
-        res.send(user)
+        const token = await user.generateAuthToken()
+        res.send({ user, token })
     } catch (error) {
         res.status(400).send()
     }
