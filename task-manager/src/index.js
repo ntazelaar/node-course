046765d5c959cb17fe9a10516ec71229829bdbb7
Nick Tazelaar <nick@nicktazelaar.com>
2021-08-05 17:31:49 +0200

    Cleanup

--- task-manager/src/index.js
+++ task-manager/src/index.js
@@ -7,35 +7,6 @@ const taskRouter = require('./routers/task')
 const app = express()
 const port = process.env.PORT || 3000
 
-const multer = require('multer')
-const upload = multer({
-    dest: 'images',
-    limits: {
-        fileSize: 1000000
-    },
-    fileFilter(req, file, callback) {
-        if (!file.originalname.match(/\.(doc|docx)$/)) {
-            return callback(new Error('Please upload a Word document'))
-        }
-
-        callback(undefined, true)
-        // cb(new Error('File must be a PDF'))
-        // cb(undefined, true)
-        // cb(undefined, false)
-    }
-})
-
-const errorMiddleware = (req, res, next) => {
-    throw new Error('From my middleware')
-}
-
-app.post('/upload', upload.single('upload'), (req, res) => {
-    res.send()
-}, (error, req, res, next) => {
-    res.status(400).send({ error: error.message })
-})
-
-
 app.use(express.json())
 app.use(userRouter)
 app.use(taskRouter)
