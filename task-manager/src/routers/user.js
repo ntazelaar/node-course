36806da76fec94c44fcd9a33899e7962263eac24
Nick Tazelaar <nick@nicktazelaar.com>
2021-08-05 17:19:55 +0200

    Completed Section 14 Lesson 6

--- task-manager/src/routers/user.js
+++ task-manager/src/routers/user.js
@@ -85,7 +85,6 @@ router.delete('/users/me', auth, async (req, res) => {
 })
 
 const avatarUpload = multer({
-    dest: 'avatars',
     limits: {
         fileSize: 1000000
     },
@@ -98,10 +97,18 @@ const avatarUpload = multer({
     }
 })
 
-router.post('/users/me/avatar', avatarUpload.single('avatar'), async (req, res) => {
+router.post('/users/me/avatar', auth, avatarUpload.single('avatar'), async (req, res) => {
+    req.user.avatar = req.file.buffer
+    await req.user.save()
     res.send()
 }, (error, req, res, next) => {
     res.status(400).send({ error: error.message })
+})
+
+router.delete('/users/me/avatar', auth, async (req, res) => {
+    req.user.avatar = undefined
+    await req.user.save()
+    res.send()
 })
 
 module.exports = router
