5947976522a642990f27e250b6339e946b212bfe
Nick Tazelaar <nick@nicktazelaar.com>
2021-08-09 19:04:21 +0200

    Completed Section 17 Lesson 7

--- chat-app/public/js/chat.js
+++ chat-app/public/js/chat.js
@@ -1,10 +1,14 @@
 const socket = io()
 
-socket.on('countUpdated', (count) => {
-    console.log('The count has been updated!', count)
+socket.on('message', (message) => {
+    console.log(message)
 })
 
-document.querySelector('#increment').addEventListener('click', () => {
-    console.log('Clicked')
-    socket.emit('increment')
+const messageForm = document.querySelector('#message-form')
+
+messageForm.addEventListener('submit', (event) => {
+    event.preventDefault()
+
+    const message = event.target.elements.message.value
+    socket.emit('sendMessage', message)
 })
