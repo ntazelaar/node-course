878c740eb6aec3c83b1ffbf5e6074697d1540ed7
Nick Tazelaar <nick@nicktazelaar.com>
2021-08-09 13:21:46 +0200

    Completed Section 16 Lesson 14

--- task-manager/tests/task.test.js
+++ task-manager/tests/task.test.js
@@ -2,7 +2,7 @@ const request = require('supertest')
 require('../src/db/mongoose')
 const app = require('../src/app')
 const Task = require('../src/models/task')
-const { userOneId, userOne, setupDatabase } = require('./fixtures/db')
+const { userOneId, userOne, setupDatabase, userTwo, taskOne } = require('./fixtures/db')
 
 beforeEach(setupDatabase)
 
@@ -18,4 +18,25 @@ test('Should create task for user', async () => {
     const task = await Task.findById(response.body._id)
     expect(task).not.toBeNull()
     expect(task.completed).toEqual(false)
+})
+
+test('Should return all tasks for user one', async () => {
+    const response = await request(app)
+        .get('/tasks')
+        .set('Authorization', `Bearer ${userOne.tokens[0].token}`)
+        .send()
+        .expect(200)
+
+    expect(response.body.length).toEqual(2)
+})
+
+test('Should not delete task that is owned by another user', async () => {
+    await request(app)
+        .delete('/tasks/' + taskOne._id)
+        .set('Authorization', `Bearer ${userTwo.tokens[0].token}`)
+        .send()
+        .expect(404)
+
+    const task = await Task.findById(taskOne._id)
+    expect(task).not.toBeNull()
 })
