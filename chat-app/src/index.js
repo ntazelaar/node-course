a87b265d2ac1b856a3abc723ca6cf6e63209e1e5
Nick Tazelaar <nick@nicktazelaar.com>
2021-08-09 14:19:20 +0200

    Completed Section 17 Lesson 4

--- chat-app/src/index.js
+++ chat-app/src/index.js
@@ -1,7 +1,11 @@
 const express = require('express')
 const path = require('path')
+const http = require('http')
+const socketio = require('socket.io')
 
 const app = express()
+const server = http.createServer(app)
+const io = socketio(server)
 
 const port = process.env.PORT || 3000
 
@@ -9,6 +13,10 @@ const publicDirectoryPath = path.join(__dirname, '../public')
 
 app.use(express.static(publicDirectoryPath))
 
-app.listen(port, () => {
+io.on('connection', () => {
+    console.log('New WebSocket connection')
+})
+
+server.listen(port, () => {
     console.log('Server listening on port ' + port)
 })
