36bc2f7a56e36b8fb98f41aa26802ce68af5a0fe
Nick Tazelaar <nick@nicktazelaar.com>
2021-08-09 19:22:07 +0200

    Completed Section 17 Lesson 8

--- chat-app/public/js/chat.js
+++ chat-app/public/js/chat.js
@@ -12,3 +12,16 @@ messageForm.addEventListener('submit', (event) => {
     const message = event.target.elements.message.value
     socket.emit('sendMessage', message)
 })
+
+document.querySelector('#send-location').addEventListener('click', () => {
+    if (!navigator.geolocation) {
+        return alert('Geolocation is not supported by your browser')
+    }
+
+    navigator.geolocation.getCurrentPosition((position) => {
+        socket.emit('sendLocation', {
+            latitude: position.coords.latitude,
+            longitude: position.coords.longitude
+        })
+    })
+})
