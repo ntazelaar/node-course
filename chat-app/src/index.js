964fb9e550a008efd9c698ccc0887ecb456ffe7e
Nick Tazelaar <nick@nicktazelaar.com>
2021-08-10 12:51:15 +0200

    Completed Section 17 Lesson 22

--- chat-app/src/index.js
+++ chat-app/src/index.js
@@ -30,6 +30,10 @@ io.on('connection', (socket) => {
 
         socket.emit('message', generateMessage('Admin', `Welcome ${user.username}!`))
         socket.broadcast.to(user.room).emit('message', generateMessage('Admin', `${user.username} has joined`))
+        io.to(user.room).emit('roomData', {
+            room: user.room,
+            users: getUsersInRoom(user.room)
+        })
 
         callback()
     })
